a26c1d2e1489edb130ab37eef475e4ff01a6118c
Elliot Lichtman <elliot.joseph.lichtman@gmail.com>
2022-09-26 21:21:36 -0700

    Highlighting Moves (colors still wonky)

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def get_most_recent_game(username):
     data = get_player_game_archives(username).json
     url = data['archives'][-1]
     games = requests.get(url).json()
-    game = games['games'][-4]
+    game = games['games'][-1]
     return game['pgn']
 
 username = input("Whose account? ")
@@ -125,6 +125,27 @@ def drawBackground():
             else:
                 color = True
 
+def drawSquareNames(screen):
+    font = pygame.font.Font("freesansbold.ttf", 10)
+
+    for i in range(8, 0, -1):
+        if i % 2 == 0:
+            text = font.render(str(i), True, (76,116,156))
+        else:
+            text = font.render(str(i), True, (236,236,212))
+        rect = text.get_rect()
+        rect.center = (5, 80*(8-i) + 5)
+        screen.blit(text, rect)
+    letters = "abcdefgh"
+    for i in range(8):
+        if i % 2 == 1:
+            text = font.render(letters[i], True, (76, 116, 156))
+        else:
+            text = font.render(letters[i], True, (236, 236, 212))
+        rect = text.get_rect()
+        rect.center = (80*(i) + 75, 635)
+        screen.blit(text, rect)
+
 # This function will draw the pieces on top of the board in the right squares. The input is the board as a string
 def drawPieces(board):
     pieceCounter = 0
@@ -166,6 +187,13 @@ def drawEval(currentEval, screen):
     rect.center = (820, 213)
     screen.blit(text, rect)
 
+def drawBestMove(currentBestMove, screen):
+    font = pygame.font.Font("freesansbold.ttf", 60)
+
+    text = font.render(str(currentBestMove), True, (0, 0, 0))
+    rect = text.get_rect()
+    rect.center = (820, 426)
+    screen.blit(text, rect)
 
 
 def convertFenToElliot(fenStr):
@@ -201,8 +229,49 @@ def convertFenToElliot(fenStr):
 print(stockfish.get_fen_position())
 currentElliot = convertFenToElliot(stockfish.get_fen_position())#"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR")
 
+def getMoveHighlight(previousMove, goodMove, computerMove):
+    previousMove = str(previousMove)
+    print(previousMove)
+    if computerMove:
+        symbol1 = "c"
+        symbol2 = "C"
+    elif goodMove:
+        symbol1 = "y"
+        symbol2 = "Y"
+    else:
+        symbol1 = "b"
+        symbol2 = "B"
+
+    letters = "abcdefgh"
+    col1 = letters.index(previousMove[0])
+    col2 = letters.index(previousMove[2])
+    row1 = 8-(int(previousMove[1]))
+    row2 = 8-(int(previousMove[3]))
+
+    print(row1, row2, col1, col2)
+
+    retStr = ""
+    found = False
+
+    for i in range(64):
+        if i != 8*row1 + col1 and i != 8 * row2 + col2:
+            retStr += "G"
+        elif not found:
+            retStr += symbol1
+            found = True
+        else:
+            retStr += symbol2
+    print(retStr)
+    return retStr
+
+
+
 currentMove = len(uci)-1
 currentEval = stockfish.get_evaluation()
+currentBestMove = stockfish.get_best_move()
+goodMove = stockfish.is_move_correct(currentMove)
+computerMove = False
+moveHighlight = getMoveHighlight(str(uci[currentMove]), goodMove, computerMove)
 
 running = True
 while running:
@@ -214,9 +283,11 @@ while running:
                 running = False
 
             if event.key == pygame.K_s:
+                moveHighlight = "GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG"
                 currentMove = 0
                 stockfish.set_position([])
                 currentEval = stockfish.get_evaluation()
+                currentBestMove = stockfish.get_best_move()
                 currentElliot = convertFenToElliot(stockfish.get_fen_position())
 
             if event.key == pygame.K_LEFT and currentMove > 0:
@@ -225,9 +296,12 @@ while running:
                 newMoves = []
                 for i in range(currentMove):
                     newMoves.append(uci[i])
-
+                pastMove = newMoves[-1]
+                goodMove = stockfish.is_move_correct(pastMove)
+                moveHighlight = getMoveHighlight(pastMove, goodMove, computerMove)
                 stockfish.set_position(newMoves)
                 currentEval = stockfish.get_evaluation()
+                currentBestMove = stockfish.get_best_move()
                 currentElliot = convertFenToElliot(stockfish.get_fen_position())
 
             if event.key == pygame.K_RIGHT and currentMove < len(uci):
@@ -237,13 +311,58 @@ while running:
                 for i in range(currentMove):
                     newMoves.append(uci[i])
 
+                try:
+                    pastMove = newMoves[-1]
+                    goodMove = stockfish.is_move_correct(pastMove)
+                    moveHighlight = getMoveHighlight(pastMove, goodMove, computerMove)
+                except:
+                    moveHighlight = "GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG"
+
                 stockfish.set_position(newMoves)
                 currentEval = stockfish.get_evaluation()
+                currentBestMove = stockfish.get_best_move()
                 currentElliot = convertFenToElliot(stockfish.get_fen_position())
+
+            if event.key == pygame.K_p:
+                try:
+                    computerMove = True
+                    newBest = stockfish.get_best_move()
+                    moveHighlight = getMoveHighlight(newBest, True, computerMove)
+                    stockfish.make_moves_from_current_position([newBest])
+                    currentEval = stockfish.get_evaluation()
+                    currentBestMove = stockfish.get_best_move()
+                    currentElliot = convertFenToElliot(stockfish.get_fen_position())
+                except:
+                    pass
+
+            if event.key == pygame.K_r:
+                stockfish.set_position(newMoves)
+                currentEval = stockfish.get_evaluation()
+                currentBestMove = stockfish.get_best_move()
+                currentElliot = convertFenToElliot(stockfish.get_fen_position())
+
 
 
 
     drawBackground()
     drawEval((currentEval['value']/100), screen)
+    drawBestMove(currentBestMove, screen)
+    if moveHighlight != "GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG":
+        if computerMove:
+            pygame.draw.rect(screen, (100, 100, 200), pygame.Rect(moveHighlight.index("c") % 8 * 80, moveHighlight.index("c") // 8 * 80, 80, 80))
+            pygame.draw.rect(screen, (100, 100, 200),
+                             pygame.Rect(moveHighlight.index("C") % 8 * 80, moveHighlight.index("C") // 8 * 80, 80, 80))
+        elif goodMove:
+            pygame.draw.rect(screen, (100, 200, 100),
+                     pygame.Rect(moveHighlight.index("y") % 8 * 80, moveHighlight.index("y") // 8 * 80, 80, 80))
+            pygame.draw.rect(screen, (100, 200, 100),
+                             pygame.Rect(moveHighlight.index("Y") % 8 * 80, moveHighlight.index("Y") // 8 * 80, 80, 80))
+        else:
+            pygame.draw.rect(screen, (200, 100, 100),
+                     pygame.Rect(moveHighlight.index("b") % 8 * 80, moveHighlight.index("b") // 8 * 80, 80, 80))
+            pygame.draw.rect(screen, (200, 100, 100),
+                             pygame.Rect(moveHighlight.index("B") % 8 * 80, moveHighlight.index("B") // 8 * 80, 80, 80))
+
+    drawSquareNames(screen)
     drawPieces(currentElliot)
     pygame.display.update()
